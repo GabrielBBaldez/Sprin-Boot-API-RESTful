c8f497643481ca1c397a7538980aa609c1c6b885
Gabriel Baldez <gabrielbbaldez@outlook.com>
2023-04-22 18:13:09 -0300

    Criação de Repository
    - Criado repository para medico
    - Criado conversão de DTO'S medico e endereço para persistir no banco de dados

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.medio.DadosCadastroMedico;
+import med.voll.api.medio.Medico;
+import med.voll.api.medio.MedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedidoRepository repository;
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+      repository.save(new Medico(dados));
+
     }
 }
